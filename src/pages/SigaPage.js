f412079b905ba0429c67076334d3461461fc61bc
forshaw <sailonbr@gmail.com>
2023-10-20 17:52:02 -0300

    ADICIONAR SISPASS DOF E PREVFOGO NO SIGAPAGE

--- src/pages/SigaPage.js
+++ src/pages/SigaPage.js
@@ -5,9 +5,6 @@ import '../css/responsive.css';
 import '../css/SigaPage.css';
 
 const SigaPage = () => {
-
-
-
     const [data, setData] = useState([
         {
             nome: 'João da Silva',
@@ -22,10 +19,11 @@ const SigaPage = () => {
                     localAutuacao: 'Reserva Ambiental',
                     artigoInfringido: 'Artigo 123',
                 },
-                // ... (outros autos fictícios)
             ],
+            sispass: ['Pássaro 1', 'Pássaro 2'],
+            dof: ['Transação 1', 'Transação 2'],
+            prevfogo: ['Incêndio 1', 'Incêndio 2'],
         },
-        // ... (outros dados fictícios)
     ]);
 
     // Carrega os dados do armazenamento local ao iniciar
@@ -41,13 +39,14 @@ const SigaPage = () => {
         localStorage.setItem('sigaData', JSON.stringify(data));
     }, [data]);
 
-
-
     const addRow = () => {
         const newRow = {
             nome: '',
             cpfCnpj: '',
             autos: [],
+            sispass: [],
+            dof: [],
+            prevfogo: [],
         };
         setData([...data, newRow]);
     };
@@ -68,11 +67,19 @@ const SigaPage = () => {
         const newData = [...data];
         newData[rowIndex].autos[autoIndex][field] = value;
         setData(newData);
-      };
+    };
 
     const addAuto = (index) => {
         const newData = [...data];
-        newData[index].autos.push({ id: Date.now(), auto: '', orgao: '' });
+        newData[index].autos.push({
+            id: Date.now(),
+            auto: '',
+            orgao: '',
+            valor: 0,
+            dataAutuacao: '',
+            localAutuacao: '',
+            artigoInfringido: '',
+        });
         setData(newData);
     };
 
@@ -82,11 +89,51 @@ const SigaPage = () => {
         setData(newData);
     };
 
+    const updateMultiLineField = (index, field, value, itemIndex) => {
+        const newData = [...data];
+        newData[index][field][itemIndex] = value;
+        setData(newData);
+    };
+
+    const addSispass = (index) => {
+        const newData = [...data];
+        newData[index].sispass.push('');
+        setData(newData);
+    };
+
+    const removeSispass = (index, sispassIndex) => {
+        const newData = [...data];
+        newData[index].sispass.splice(sispassIndex, 1);
+        setData(newData);
+    };
+
+    const addDof = (index) => {
+        const newData = [...data];
+        newData[index].dof.push('');
+        setData(newData);
+    };
+
+    const removeDof = (index, dofIndex) => {
+        const newData = [...data];
+        newData[index].dof.splice(dofIndex, 1);
+        setData(newData);
+    };
+
+    const addPrevfogo = (index) => {
+        const newData = [...data];
+        newData[index].prevfogo.push('');
+        setData(newData);
+    };
+
+    const removePrevfogo = (index, prevfogoIndex) => {
+        const newData = [...data];
+        newData[index].prevfogo.splice(prevfogoIndex, 1);
+        setData(newData);
+    };
+
     return (
         <div className="siga-page">
-
-<BackLink to="/" text="Voltar" />
-
+            <BackLink to="/" text="Voltar" />
 
             <h1>Sistema Integrado de Gestão Ambiental (SIGA)</h1>
             <table>
@@ -95,6 +142,9 @@ const SigaPage = () => {
                         <th>Nome do Autuado</th>
                         <th>CPF/CNPJ do Autuado</th>
                         <th>Autos de Infração</th>
+                        <th>SISPASS</th>
+                        <th>DOF</th>
+                        <th>PREVFOGO</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -134,25 +184,25 @@ const SigaPage = () => {
                                         <input
                                             type="number"
                                             value={auto.valor}
-                                            onChange={(e) => updateRow(rowIndex, autoIndex, 'valor', e.target.value)}
+                                            onChange={(e) => updateAutoField(rowIndex, autoIndex, 'valor', e.target.value)}
                                             placeholder="Valor (R$)"
                                         />
                                         <input
                                             type="date"
                                             value={auto.dataAutuacao}
-                                            onChange={(e) => updateRow(rowIndex, autoIndex, 'dataAutuacao', e.target.value)}
+                                            onChange={(e) => updateAutoField(rowIndex, autoIndex, 'dataAutuacao', e.target.value)}
                                             placeholder="Data de Autuação"
                                         />
                                         <input
                                             type="text"
                                             value={auto.localAutuacao}
-                                            onChange={(e) => updateRow(rowIndex, autoIndex, 'localAutuacao', e.target.value)}
+                                            onChange={(e) => updateAutoField(rowIndex, autoIndex, 'localAutuacao', e.target.value)}
                                             placeholder="Local de Autuação"
                                         />
                                         <input
                                             type="text"
                                             value={auto.artigoInfringido}
-                                            onChange={(e) => updateRow(rowIndex, autoIndex, 'artigoInfringido', e.target.value)}
+                                            onChange={(e) => updateAutoField(rowIndex, autoIndex, 'artigoInfringido', e.target.value)}
                                             placeholder="Artigo Infringido"
                                         />
                                         <button onClick={() => removeAuto(rowIndex, auto.id)}>Remover Auto</button>
@@ -160,6 +210,48 @@ const SigaPage = () => {
                                 ))}
                                 <button onClick={() => addAuto(rowIndex)}>Adicionar Auto</button>
                             </td>
+                            <td>
+                            {row.sispass.map((sispass, sispassIndex) => (
+                                <div key={sispassIndex}>
+                                    <input
+                                        type="text"
+                                        value={sispass}
+                                        onChange={(e) => updateMultiLineField(rowIndex, 'sispass', e.target.value, sispassIndex)}
+                                        placeholder="Pássaro"
+                                    />
+                                    <button onClick={() => removeSispass(rowIndex, sispassIndex)}>Remover SISPASS</button>
+                                </div>
+                            ))}
+                            <button onClick={() => addSispass(rowIndex)}>Adicionar SISPASS</button>
+                        </td>
+                        <td>
+                            {row.dof.map((dof, dofIndex) => (
+                                <div key={dofIndex}>
+                                    <input
+                                        type="text"
+                                        value={dof}
+                                        onChange={(e) => updateMultiLineField(rowIndex, 'dof', e.target.value, dofIndex)}
+                                        placeholder="Transação"
+                                    />
+                                    <button onClick={() => removeDof(rowIndex, dofIndex)}>Remover DOF</button>
+                                </div>
+                            ))}
+                            <button onClick={() => addDof(rowIndex)}>Adicionar DOF</button>
+                        </td>
+                        <td>
+                            {row.prevfogo.map((prevfogo, prevfogoIndex) => (
+                                <div key={prevfogoIndex}>
+                                    <input
+                                        type="text"
+                                        value={prevfogo}
+                                        onChange={(e) => updateMultiLineField(rowIndex, 'prevfogo', e.target.value, prevfogoIndex)}
+                                        placeholder="Incêndio"
+                                    />
+                                    <button onClick={() => removePrevfogo(rowIndex, prevfogoIndex)}>Remover PREVFOGO</button>
+                                </div>
+                            ))}
+                            <button onClick={() => addPrevfogo(rowIndex)}>Adicionar PREVFOGO</button>
+                        </td>
                             <td>
                                 <button onClick={() => removeRow(rowIndex)}>Remover Linha</button>
                             </td>
@@ -170,8 +262,6 @@ const SigaPage = () => {
             <button onClick={addRow}>Adicionar Linha</button>
         </div>
     );
-
 };
-
 
 export default SigaPage;
